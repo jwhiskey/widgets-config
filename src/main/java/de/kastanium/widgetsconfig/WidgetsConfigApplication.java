524bc9cc209ab3db8c0116292a506f34b6c3789f
jimmiwhiskey <jimmiwhiskey@gmail.com>
2025-12-28 19:52:11 +0100

    initial commit...

---
+++ src/main/java/de/kastanium/widgetsconfig/WidgetsConfigApplication.java
@@ -0,0 +1,13 @@
+package de.kastanium.widgetsconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetsConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WidgetsConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kastanium/widgetsconfig/WidgetsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kastanium.widgetsconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetsConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
